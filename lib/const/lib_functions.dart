3ea3fc84a1aadc86705aecfddc97b1f57246cf71
Khalid Meftu <90188748+KhalidMeftu@users.noreply.github.com>
2023-05-21 20:47:00 +0300

    convertor function logic implementation

--- lib/const/lib_functions.dart
+++ lib/const/lib_functions.dart
@@ -1,16 +1,17 @@
-
 import 'package:abushakir/abushakir.dart';
 
 /// contains constant functions for our library
 
-
- ethiopianToGregorianDateConvertor(int day,ETC ethiopianCalender ){
-   /// this function will be called when we want to display
-   /// gregorian calender with ethiopian calender
-   /// it takes ethiopian calender and date as a param and returns gregorian calender
-   EtDatetime ethiopianDateTime = EtDatetime(year: ethiopianCalender.year, month: ethiopianCalender.month, day: day);
-   DateTime gregorianCalender = DateTime.fromMillisecondsSinceEpoch(ethiopianDateTime.moment);
-   String result =  gregorianCalender.toString().substring(0, gregorianCalender.toString().indexOf(' '));
-   var gregorianConvertedDate = result.split('-');
-   return gregorianConvertedDate[2].toString().replaceAll(RegExp(r'^0+(?=.)'), '');
+ethiopianToGregorianDateConvertor(
+    int day, ETC ethiopianCalender, bool forCalender) {
+  /// this function will be called when we want to display
+  /// gregorian calender with ethiopian calender
+  /// it takes ethiopian calender and date as a param and returns gregorian calender
+  EtDatetime ethiopianDateTime = EtDatetime(year: ethiopianCalender.year, month: ethiopianCalender.month, day: day);
+  DateTime gregorianCalender = DateTime.fromMillisecondsSinceEpoch(ethiopianDateTime.moment);
+  String result = gregorianCalender.toString().substring(0, gregorianCalender.toString().indexOf(' '));
+  var gregorianConvertedDate = result.split('-');
+  return forCalender
+      ? gregorianConvertedDate[2].toString().replaceAll(RegExp(r'^0+(?=.)'), '')
+      : result;
 }
